c4a5acb5bd82c3bafcf72d9ae20bc59d990c63d9
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2025-07-08 12:07:31 -0300

    build: estruturando o frontend

---
+++ web/src/app.tsx
@@ -0,0 +1,20 @@
+/** biome-ignore-all assist/source/organizeImports: dont sort imports */
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+
+const queryClient = new QueryClient();
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
